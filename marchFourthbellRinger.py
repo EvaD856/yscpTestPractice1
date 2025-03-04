386af03fb2e3d0a6e8775239da618a1cde49f44b
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2025-03-04 13:56:30 +0000

    correction

--- marchFourthbellRinger.py
+++ marchFourthbellRinger.py
@@ -6,7 +6,7 @@ myDict = {}
 #Asks for name and a grade 10 times
 for i in range(10):
     name = input("Enter a student name: ")
-    grade = integer(input("Enter the student's grade: "))
+    grade = int(input("Enter the student's grade: "))
     myDict [name] = grade
 
 #prints out names and grades
