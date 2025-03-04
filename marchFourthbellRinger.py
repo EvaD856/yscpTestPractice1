31787439691e4eb83099ac73666375977214c78d
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2025-03-04 13:47:40 +0000

    3/4/25 Bellringer

---
+++ marchFourthbellRinger.py
@@ -0,0 +1,13 @@
+#did in python repository and copied and pasted into this repository so easy to find
+
+# Sets empty dictionary
+myDict = {}
+
+#Asks for name and a grade 10 times
+for i in range(10):
+    name = input("Enter a student name")
+    grade = input("Enter the student's grade")
+    myDict [name] = grade
+
+#prints out names and grades
+print (myDict)
